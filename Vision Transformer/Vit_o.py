0a3d7933401cb97127744bb18f50560b96d445cc
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2022-05-31 12:31:11 +0100

    Add files via upload

---
+++ Vision Transformer/Vit_o.py
@@ -0,0 +1,98 @@
+import torch.nn as nn
+import torch
+def img_to_patch(x, patch_size, flatten_channels=True):
+    """
+    Inputs:
+        x - torch.Tensor representing the image of shape [B, C, H, W]
+        patch_size - Number of pixels per dimension of the patches (integer)
+        flatten_channels - If True, the patches will be returned in a flattened format
+                           as a feature vector instead of a image grid.
+    """
+    B, C, H, W = x.shape
+    x = x.reshape(B, C, H//patch_size, patch_size, W//patch_size, patch_size)
+    x = x.permute(0, 2, 4, 1, 3, 5) # [B, H', W', C, p_H, p_W]
+    x = x.flatten(1,2)              # [B, H'*W', C, p_H, p_W]
+    if flatten_channels:
+        x = x.flatten(2,4)          # [B, H'*W', C*p_H*p_W]
+    return x
+class AttentionBlock(nn.Module):
+
+    def __init__(self, embed_dim, hidden_dim, num_heads, dropout=0.0):
+        """
+        Inputs:
+            embed_dim - Dimensionality of input and attention feature vectors
+            hidden_dim - Dimensionality of hidden layer in feed-forward network
+                         (usually 2-4x larger than embed_dim)
+            num_heads - Number of heads to use in the Multi-Head Attention block
+            dropout - Amount of dropout to apply in the feed-forward network
+        """
+        super().__init__()
+
+        self.layer_norm_1 = nn.LayerNorm(embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads)
+        self.layer_norm_2 = nn.LayerNorm(embed_dim)
+        self.linear = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, embed_dim),
+            nn.Dropout(dropout)
+        )
+
+
+    def forward(self, x):
+        inp_x = self.layer_norm_1(x)
+        x = x + self.attn(inp_x, inp_x, inp_x)[0]
+        x = x + self.linear(self.layer_norm_2(x))
+        return x
+class VisionTransformer(nn.Module):
+
+    def __init__(self, embed_dim, hidden_dim, num_channels, num_heads, num_layers, num_classes, patch_size, num_patches, dropout=0.2):
+        super().__init__()
+
+        self.patch_size = patch_size
+
+        # Layers/Networks
+        self.input_layer = nn.Linear(num_channels*(patch_size**2), embed_dim)
+        self.transformer = nn.Sequential(*[AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)])
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim, num_classes)
+        )
+        self.dropout = nn.Dropout(dropout)
+
+        # Parameters/Embeddings
+        self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
+        self.pos_embedding = nn.Parameter(torch.randn(1,1+num_patches,embed_dim))
+
+
+    def forward(self, x):
+        # Preprocess input
+        x = img_to_patch(x, self.patch_size)
+        B, T, _ = x.shape
+        x = self.input_layer(x)
+
+        # Add CLS token and positional encoding
+        cls_token = self.cls_token.repeat(B, 1, 1)
+        x = torch.cat([cls_token, x], dim=1)
+        x = x + self.pos_embedding[:,:T+1]
+        # Apply Transforrmer
+        x = self.dropout(x)
+        x = x.transpose(0, 1)
+        x = self.transformer(x)
+
+        # Perform classification prediction
+        cls = x[0]
+        out = self.mlp_head(cls)
+        return out
+# def model() -> VisionTransformer:
+#      model = VisionTransformer (embed_dim=256, hidden_dim=512,num_channels=1, num_heads=8, num_layers=6, num_classes=10, patch_size=4, num_patches=144)
+#      return model
+
+
+# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# from torchsummary import summary
+# model = model()
+# model.to(device=DEVICE,dtype=torch.float)
+# summary(model, (1,48,48) )
---
+++ Vision Transformer/read_data1.py
@@ -0,0 +1,33 @@
+
+import torch
+from torchvision.datasets import CIFAR10
+DATASET_PATH='/data/home/acw676/cifar10/'
+from torchvision import transforms
+import pytorch_lightning as pl
+import torch.utils.data as data
+
+test_transform = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+                                     ])
+# For training, we add some augmentation. Networks are too powerful and would overfit.
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                      transforms.RandomResizedCrop((32,32),scale=(0.8,1.0),ratio=(0.9,1.1)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+                                     ])
+# Loading the training dataset. We need to split it into a training and validation part
+# We need to do a little trick because the validation set should not use the augmentation.
+train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
+val_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=test_transform, download=True)
+pl.seed_everything(42)
+train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
+pl.seed_everything(42)
+_, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
+
+# Loading the test set
+test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
+
+# We define a set of data loaders that we can use for various purposes later.
+train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
+val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
